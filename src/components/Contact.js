265907c398ab4c089ae18f7c267222c33653a311
pwsmith4 <97992667+pwsmith4@users.noreply.github.com>
2024-05-05 17:45:57 -0700

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -18,7 +18,6 @@ export default function Contact() {
 
     function handleSubmit(e) {
       console.log("submitting form");
-      sendEmail(e);
       const sendEmail = (e) => {
       e.preventDefault();
     
@@ -74,7 +73,7 @@ export default function Contact() {
         <form
           netlify
           ref={form}
-          onSubmit={handleSubmit}
+          onSubmit={sendEmail}
           name="contact"
           className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
           <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
