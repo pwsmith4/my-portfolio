ba4209aa4082f8e328e4362b7e0e9c026609d771
pwsmith4 <97992667+pwsmith4@users.noreply.github.com>
2024-05-06 12:56:37 -0700

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -32,6 +32,22 @@ export default function About() {
           />
         </div>
       </div>
+      <div>
+        <h1 className="sm:text-4xl text-3xl font-medium title-font text-white mb-4">
+          About Me
+        </h1>
+        <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">
+          I'm a software engineer based in Folsom, CA specializing in building
+          and designing exceptional websites, applications, and
+          everything in between.
+        </p>
+        <a href="https://github.com/pwsmith4/resume/blob/1e0c8a24a87bc030457768326a5ce4719fcc3d44/Smith%20Parker%20Resume.pdf" target="_blank" rel="noopener noreferrer" className="text-blue-500 underline">
+      View my resume
+    </a>
+    <a href="https://www.linkedin.com/in/parkerwsmith/" target="_blank" rel="noopener noreferrer" className="text-blue-500 underline">
+      Visit my LinkedIn
+    </a>
+      </div>
     </section>
   );
 }
