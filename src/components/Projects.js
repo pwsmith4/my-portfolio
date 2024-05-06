4c629b598628cdc84930afa170900ddc0a4c5ead
pwsmith4 <97992667+pwsmith4@users.noreply.github.com>
2024-05-06 10:20:35 -0700

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -21,7 +21,9 @@ export default function Projects() {
             <div className={idx === 0 ? "sm:col-span-3 p-4" : "p-4 h-full"}>
               <a href={project.link} key={project.image}>
                 <div className="relative h-full">
-                  <h1 className="mb-2">{project.mainTitle}</h1>
+                <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-white mb-2">
+                  {project.mainTitle}
+                  </h1>
                   <img
                     alt="gallery"
                     className="w-full h-full object-cover object-center object-left" // Add object-right here
